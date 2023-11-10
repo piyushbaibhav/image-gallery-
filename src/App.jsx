d930361f1063e56e93bbe24691980777700dc5cd
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-10 18:33:21 +0530

    only image file upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './assets/Title';
+import UploadForm from './assets/UploadForm';
 
 function App() {
   return (
     <div className="max-w-960 mx-auto">
       <Title/>
+      <UploadForm />
     </div>
   );
 }
---
+++ src/assets/UploadForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const UploadForm = ()=>{
+
+    const changeHandler =(e)=>{
+        const [file, setFile] = useState(null);
+
+        const types = ['image/png','image/jpeg','image/gif']
+        
+        let selected = e.target.files[0];
+        // console.log(selected);
+        if(selected){
+            setFile(selected);
+        }
+    }
+
+    return(
+        <form>
+            <input type='file' onChange={changeHandler} />
+        </form>
+    )
+}
+
+export default UploadForm;
