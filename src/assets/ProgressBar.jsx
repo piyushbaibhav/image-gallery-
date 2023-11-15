943b4f5bb36335481758921b067975f1b2f762ec
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-16 00:15:51 +0530

    removed firebase

--- src/assets/ProgressBar.jsx
+++ src/assets/ProgressBar.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import useStorage from "../hooks/useStorage";
 
 
-const ProgressBar = () =>{
+const ProgressBar = ({ file, setFile}) => {
+    const {url, progress} = useStorage(file);
+    console.log(progress, url);
 
     return (
         <div className="progress-bar">progress</div>
