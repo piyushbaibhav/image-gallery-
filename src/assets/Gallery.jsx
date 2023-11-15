56db562b93ceeb8fcfb34c9f7ae1c3ae424c05d4
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-16 00:38:33 +0530

    basic

--- src/assets/Gallery.jsx
+++ src/assets/Gallery.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+// Gallery.js
+import React, { useState } from 'react';
+import Image from './Image';
+import FileUpload from './FileUpload';
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
+  const [uploadedImages, setUploadedImages] = useState([]);
 
-export default Gallery
+  const handleFileUpload = (files) => {
+    const newImages = files.map((file) => ({
+      src: URL.createObjectURL(file),
+      alt: file.name,
+    }));
+
+    setUploadedImages([...uploadedImages, ...newImages]);
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Image Gallery</h1>
+      <FileUpload onFileUpload={handleFileUpload} />
+      <div className="grid grid-cols-3 gap-4">
+        {uploadedImages.map((image, index) => (
+          <Image key={index} src={image.src} alt={image.alt} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
