b208fa87933c34fc16b7be4614903ec30dbf2816
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-11-16 00:23:51 +0530

    image

--- src/assets/Image.jsx
+++ src/assets/Image.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Image = () => {
-  return (
-    <div>Image</div>
-  )
-}
+const Image = ({ src, alt }) => {
+  return <img src={src} alt={alt} className="rounded-md mb-4" />;
+};
 
-export default Image
+export default Image;
